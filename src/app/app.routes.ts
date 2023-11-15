dcf820dbb263b9e15285c501a3c2ff4da5736b5d
diaba <hwar@cincinnatistate.edu>
2023-11-15 16:14:22 -0500

    add routes and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { BookingComponent } from './booking/booking.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contactus', component: ContactusComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
